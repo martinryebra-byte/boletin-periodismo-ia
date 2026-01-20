2a99a8d8f45f5de63c1354d5f4a2417b2a691409
martinryebra-byte <martinryebra@gmail.com>
2026-01-20 11:32:42 -0300

    Create sources.py

---
+++ sources.py
@@ -0,0 +1,16 @@
+RSS_FEEDS = [
+    # Tier 1
+    "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+    "https://www.theguardian.com/world/rss",
+    "https://www.lemonde.fr/rss/une.xml",
+    "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada",
+    "https://www.lanacion.com.ar/arc/outboundfeeds/rss/",
+    "https://www.ft.com/rss/home",
+    "https://www.reuters.com/rssFeed/topNews",
+
+    # Alternativos
+    "https://www.axios.com/feeds/feed.rss",
+    "https://www.vox.com/rss/index.xml",
+    "https://www.wired.com/feed/rss",
+    "https://www.propublica.org/feeds/propublica/main"
+]
